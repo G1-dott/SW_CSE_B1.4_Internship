f2ade722ff8030116278dd2adaef6d8b32cfbd2c
G1-dott <3.ojeevan@gmail.com>
2026-07-29 23:37:42 +0530

    Add Day4 to Day13 internship assignments

---
+++ Day8/p4.js
@@ -0,0 +1,46 @@
+var x = 10
+var x = 20
+x = 30
+console.log(x)
+
+if(true){
+    var message = "Hello"
+}
+//var is not block scoped
+console.log(message)
+
+//let
+let score = 100
+// let score = 200 ERROR
+score = 160
+
+console.log(score)
+
+if(true){
+    let secret = "hello"
+}
+//let is block scoped
+console.log(secret)
+
+//const
+if(true){
+    const pi=3.1415
+    // pi=22 ERROR
+}
+//const is block scoped
+console.log(pi) //ERROR
+
+
+//var function scoped
+function test(){
+    if(true){
+        var a = 10
+        let b = 20
+        const c = 30
+    }
+    console.log(a)
+    console.log(b)  //ERROR
+    console.log(c) //ERROR
+}
+// console.log(a) ERROR
+test()
